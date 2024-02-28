ef9af18c19a3f624ffa571e7bfcfdb7c9b7b28bc
FrancescaBarberio <barberiofrancesca@hotmail.it>
2024-02-28 10:57:49 +0100

    add project

---
+++ src/test/java/com/corso/vetrina/VetrinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corso.vetrina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetrinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
